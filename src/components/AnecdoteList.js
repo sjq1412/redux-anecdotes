b02b16685ccd29c17c6e0c3c84fb343574ea5b5a
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-27 21:30:08 +0800

    separate list to AnecdoteList component

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const makeVote = (id) => {
+    console.log('vote', id)
+    dispatch(vote(id))
+  }
+
+  return (
+    <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.sort((a,b) => b.votes - a.votes).map(anecdote =>
+            <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => makeVote(anecdote.id)}>vote</button>
+            </div>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default AnecdoteList
