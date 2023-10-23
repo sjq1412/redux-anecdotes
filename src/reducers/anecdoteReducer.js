b41372c4f47d9be742894f61ff0baec69fdd0b68
sjq1412 <sarahdev.cod3@gmail.com>
2023-10-23 22:11:08 +0800

    use redux-thunk to initialize anecdotes

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+import anecdoteService from "../services/anecdotes"
+
 const anecdoteSlice = createSlice({
   name: "anecdotes",
   initialState: [],
@@ -24,4 +26,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, vote, setAnecdotes, addAnecdote } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
