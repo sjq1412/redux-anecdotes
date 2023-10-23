2b5596603137484919cbd0faa317c8166ba0d5b0
sjq1412 <sarahdev.cod3@gmail.com>
2023-10-23 21:31:52 +0800

    fetch anecdotes from the backend

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,11 +10,9 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const anecdote = action.payload
@@ -37,40 +26,12 @@ const anecdoteSlice = createSlice({
                 : {...anecdote, votes: anecdote.votes + 1 }
       })
       return anecdotes
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-// const reducer = (state = initialState, action) => {
-//   console.log('state now: ', state)
-//   console.log('action', action)
-
-//   switch(action.type) {
-//     case "VOTE":
-//       return state.map(anecdote => {
-//         return anecdote.id !== action.payload.id 
-//             ? anecdote 
-//             : {...anecdote, votes: anecdote.votes + 1}})
-//     case "ADD_ANECDOTE":
-//       return state.concat(action.payload)
-//     default:
-//       return state
-//   }
-// }
-
-// export const createAnecdote = anecdote => {
-//   return {
-//     type: "ADD_ANECDOTE",
-//     payload: asObject(anecdote)
-//   }
-// }
-
-// export const vote = id => {
-//   return {
-//     type: "VOTE",
-//     payload: { id }
-//   }
-// }
-
-export const { createAnecdote, vote } = anecdoteSlice.actions
+export const { createAnecdote, vote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
